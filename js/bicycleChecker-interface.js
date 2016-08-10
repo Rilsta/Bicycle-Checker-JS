6e5281b91b7d9b87121e69c374b8f9ce331dd142
rilsta <rstarnes9@gmail.com>
2016-08-10 13:34:02 -0700

    add color searching for bikes

--- js/bicycleChecker-interface.js
+++ js/bicycleChecker-interface.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
   var currentBicycleObject = new BicycleChecker();
   $('#submit-button').click(function() {
     var bikeZip = $('#bike-zip').val();
-    // var bikeRadius = $('#bike-radius').val();
+    var bikeRadius = $('#bike-radius').val();
+    var bikeColor = $('#bike-color').val();
     $('#bike-zip').val("");
-    // $('#bike-radius').val("");
-    currentBicycleObject.getBicycle(bikeZip)
+    $('#bike-radius').val("");
+    $('#bike-color').val("");
+    $('#title').append("Bikes stolen within " + bikeRadius + " miles of " + bikeZip);
+    currentBicycleObject.getBicycle(bikeZip, bikeRadius, bikeColor)
   });
 });
--- js/bicycleChecker.js
+++ js/bicycleChecker.js
@@ -1,12 +1,11 @@
 function BicycleChecker() {
 }
 
-BicycleChecker.prototype.getBicycle = function(bikeZip) {
- $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + bikeZip).then(function(response) {
-
+BicycleChecker.prototype.getBicycle = function(bikeZip, bikeRadius, bikeColor) {
+ $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + bikeZip + '&proximity_square=' + bikeRadius + '&colors=' + bikeColor).then(function(response) {
    var bikeMakes = response.bikes;
    bikeMakes.forEach(function(bike) {
-     $('#bikes').append('<li>' + bike.manufacturer_name + ", " + bike.frame_colors + '</li>');
+    $('#bikes').append('<li>' + bike.manufacturer_name + ", " + bike.frame_colors + '</li>');
    });
    console.log(bikeMakes);
  }).fail(function(error) {
