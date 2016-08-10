c195691e83d7e9e1c8325e89973131f752a4c333
rilsta <rstarnes9@gmail.com>
2016-08-10 12:10:05 -0700

    add request to the API for a response

--- js/bicycleChecker.js
+++ js/bicycleChecker.js
@@ -1,3 +1,25 @@
 function BicycleChecker() {
-  
 }
+
+BicycleChecker.prototype.getBicycle = function(bikeZip) {
+ $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + bikeZip).then(function(response) {
+   console.log(response);
+ }).fail(function(error) {
+   $('.results').text('didnt work')
+ });
+}
+
+exports.bicycleModule = BicycleChecker;
+
+function Weather() {
+}
+
+Weather.prototype.getWeather = function(city) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    $('.showWeather').text('The humidity in ' + city + ' is ' + response.main.humidity + "%");
+  }).fail(function(error) {
+    $('.showWeather').text("Whoops");
+  });
+}
+
+exports.weatherModule = Weather;
