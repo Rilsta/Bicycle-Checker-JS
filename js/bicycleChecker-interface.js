3fa446018cd51ad8c6232fbeaf7206c21ae32666
rilsta <rstarnes9@gmail.com>
2016-08-10 12:50:50 -0700

    add ability to enter a zipcode and see bike manufacturers stolen in that area

--- js/bicycleChecker-interface.js
+++ js/bicycleChecker-interface.js
@@ -1,22 +1,12 @@
+var BicycleChecker = require('./../js/bicycleChecker.js').bicycleModule;
+
 $(document).ready(function() {
+  var currentBicycleObject = new BicycleChecker();
   $('#submit-button').click(function() {
     var bikeZip = $('#bike-zip').val();
-    var bikeRadius = $('#bike-radius').val();
+    // var bikeRadius = $('#bike-radius').val();
     $('#bike-zip').val("");
-    $('#bike-radius').val("");
-    console.log(bikeRadius);
-    console.log(bikeZip);
-  });
-});
-
-
-var Weather = require('./../js/weather.js').weatherModule;
-
-$(document).ready(function() {
-  var currentWeatherObject = new Weather();
-  $('#weatherLocation').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    currentWeatherObject.getWeather(city);
+    // $('#bike-radius').val("");
+    currentBicycleObject.getBicycle(bikeZip)
   });
 });
--- js/bicycleChecker.js
+++ js/bicycleChecker.js
@@ -3,23 +3,15 @@ function BicycleChecker() {
 
 BicycleChecker.prototype.getBicycle = function(bikeZip) {
  $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + bikeZip).then(function(response) {
-   console.log(response);
+
+   var bikeMakes = response.bikes;
+   bikeMakes.forEach(function(make) {
+     $('#manufacturer').append('<li>' + make.manufacturer_name + '</li>');
+   });
+
  }).fail(function(error) {
    $('.results').text('didnt work')
  });
 }
 
 exports.bicycleModule = BicycleChecker;
-
-function Weather() {
-}
-
-Weather.prototype.getWeather = function(city) {
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-    $('.showWeather').text('The humidity in ' + city + ' is ' + response.main.humidity + "%");
-  }).fail(function(error) {
-    $('.showWeather').text("Whoops");
-  });
-}
-
-exports.weatherModule = Weather;
