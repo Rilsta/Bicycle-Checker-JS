aa5546ed42e8dbdcf5188e97964cdca5e5773a20
rilsta <rstarnes9@gmail.com>
2016-08-10 13:07:48 -0700

    show the colors of the bike next to the make

--- js/bicycleChecker.js
+++ js/bicycleChecker.js
@@ -5,10 +5,10 @@ BicycleChecker.prototype.getBicycle = function(bikeZip) {
  $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + bikeZip).then(function(response) {
 
    var bikeMakes = response.bikes;
-   bikeMakes.forEach(function(make) {
-     $('#manufacturer').append('<li>' + make.manufacturer_name + '</li>');
+   bikeMakes.forEach(function(bike) {
+     $('#bikes').append('<li>' + bike.manufacturer_name + ", " + bike.frame_colors + '</li>');
    });
-
+   console.log(bikeMakes);
  }).fail(function(error) {
    $('.results').text('didnt work')
  });
