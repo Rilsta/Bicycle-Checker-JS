9cf3f47f8dade9d6a355265899e9c7e37849c6b0
rilsta <rstarnes9@gmail.com>
2016-08-11 12:22:37 -0700

    add another get request to get bike counts

--- js/bicycleChecker-interface.js
+++ js/bicycleChecker-interface.js
@@ -3,13 +3,16 @@ var BicycleChecker = require('./../js/bicycleChecker.js').bicycleModule;
 $(document).ready(function() {
   var currentBicycleObject = new BicycleChecker();
   $('#submit-button').click(function() {
+    $('#title').empty();
     var bikeZip = $('#bike-zip').val();
     var bikeRadius = $('#bike-radius').val();
     var bikeColor = $('#bike-color').val();
+    var bikeManu = $('#bike-manufacturer').val();
     $('#bike-zip').val("");
     $('#bike-radius').val("");
     $('#bike-color').val("");
+    $('#bike-manufacturer').val("");
     $('#title').append("Bikes stolen within " + bikeRadius + " miles of " + bikeZip);
-    currentBicycleObject.getBicycle(bikeZip, bikeRadius, bikeColor)
+    currentBicycleObject.getBicycle(bikeZip, bikeRadius, bikeColor, bikeManu)
   });
 });
--- js/bicycleChecker.js
+++ js/bicycleChecker.js
@@ -1,16 +1,27 @@
 function BicycleChecker() {
 }
 
-BicycleChecker.prototype.getBicycle = function(bikeZip, bikeRadius, bikeColor) {
- $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + bikeZip + '&proximity_square=' + bikeRadius + '&colors=' + bikeColor).then(function(response) {
+BicycleChecker.prototype.getBicycle = function(bikeZip, bikeRadius, bikeColor, bikeManu) {
+
+  /*Bike Count Get Request*/
+  $.get('https://bikeindex.org:443/api/v2/bikes_search/count?page=1&proximity=' + bikeZip + '&proximity_square=' + bikeRadius + '&colors=' + bikeColor + '&manufacturer=' + bikeManu).then(function(response) {
+    $('#number-stolen').empty();
+    $('#number-stolen').append(response.proximity);
+  }).fail(function(error) {
+    $('.results').text('didnt work')
+  });
+}
+
+  /*Bike Info Get Request*/
+ $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + bikeZip + '&proximity_square=' + bikeRadius + '&colors=' + bikeColor + '&manufacturer=' + bikeManu).then(function(response) {
+   $('#bikes').empty();
    var bikeMakes = response.bikes;
    bikeMakes.forEach(function(bike) {
     $('#bikes').append('<li>' + bike.manufacturer_name + ", " + bike.frame_colors + '</li>');
    });
-   console.log(bikeMakes);
  }).fail(function(error) {
    $('.results').text('didnt work')
  });
-}
+
 
 exports.bicycleModule = BicycleChecker;
